5f530fe1e9530f422419589fb2083655265eb508
Noble-01 <zaccripps5@gmail.com>
2020-03-19 15:05:54 +1000

    seperate folders and files in a list

--- cleanDesktop.py
+++ cleanDesktop.py
@@ -1,6 +1,11 @@
 import os
 
 directory = ".."
+print("folders")
 for entry in os.listdir(directory):
     if os.path.isdir(os.path.join(directory, entry)):
+        print(entry)
+print("\n files")
+for entry in os.listdir(directory):
+    if os.path.isfile(os.path.join(directory, entry)):
         print(entry)
