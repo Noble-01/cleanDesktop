f630f9295e5f4f59d3329678299951d624939350
Noble-01 <zaccripps5@gmail.com>
2020-03-19 15:48:39 +1000

    check for folder already exists and create folder

--- cleanDesktop.py
+++ cleanDesktop.py
@@ -1,8 +1,14 @@
 import os
 folders = []
 files =[]
-
 directory = ".."
+checkDir = "../Folders"
+if not os.path.exists(checkDir):
+    os.mkdir(checkDir)
+    print("directory", checkDir , "created")
+else:
+    print("directory already exists")
+
 for folder in os.listdir(directory):
     if os.path.isdir(os.path.join(directory, folder)):
         folders.append(folder)
